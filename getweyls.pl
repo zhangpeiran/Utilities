e1e75cf4b19ac6e10d10c98f51c16a20fc17e226
Przhang <przhang@zju.edu.cn>
2018-01-20 16:52:44 +0800

    revise

--- getweyls.pl
+++ getweyls.pl
@@ -1,9 +1,13 @@
 #!/usr/bin/perl
+#
+$command=readpipe("wc -l ./WT.out");
+@buffer=split(/\s/,$command);
+$linenumber=$buffer[0];
 open(FILE,"WT.out");
 while($_=<FILE>) {
 if(/Chiralities/)
 {
-for($i=0;$i<3768;$i++)
+for($i=0;$i<$linenumber;$i++)
 {
 $_=<FILE>;
 @line=split;
